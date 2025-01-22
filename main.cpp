92634c2912397dcf80a682bc48452e867d04e6ed
Everton Cavalcante <evertonranielly@gmail.com>
2025-01-21 21:30:54 -0300

    Simplifying main

--- main.cpp
+++ main.cpp
@@ -7,13 +7,7 @@ int counter = 0;
 
 void incrementCounter() {
     for (int i = 1; i <= 1000; i++) {
-        int time = rand() % 10 + 1;
-        int temp = counter;
-        // Random delay to make it easier to observe the effects of race
-        // conditions in practice
-        std::this_thread::sleep_for(std::chrono::microseconds(time));
-        temp = temp + 1;
-        counter = temp;
+        counter++;
     }
 }
 
