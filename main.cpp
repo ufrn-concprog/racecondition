f22b46b4a48980f468d3d305fd7f8f83a4c2ae38
Everton Cavalcante <evertonranielly@gmail.com>
2025-01-21 21:23:43 -0300

    Minor changes

--- main.cpp
+++ main.cpp
@@ -6,26 +6,27 @@
 int counter = 0;
 
 void incrementCounter() {
-  for (int i = 1; i <= 1000; i++) {
-    int time = rand() % 10 + 1;
-    int temp = counter;
-    // Random delay to make it easier to observe the effects of race conditions in practice
-    std::this_thread::sleep_for(std::chrono::microseconds(time));
-    temp = temp + 1;
-    counter = temp;
-  }
+    for (int i = 1; i <= 1000; i++) {
+        int time = rand() % 10 + 1;
+        int temp = counter;
+        // Random delay to make it easier to observe the effects of race
+        // conditions in practice
+        std::this_thread::sleep_for(std::chrono::microseconds(time));
+        temp = temp + 1;
+        counter = temp;
+    }
 }
 
 int main() {
-  srand(time(NULL));
+    srand(time(NULL));
 
-  std::thread t1(incrementCounter);
-  std::thread t2(incrementCounter);
+    std::thread t1(incrementCounter);
+    std::thread t2(incrementCounter);
 
-  t1.join();
-  t2.join();
+    t1.join();
+    t2.join();
 
-  std::cout << "Final value of counter: " << counter << std::endl;
+    std::cout << "Final value of counter: " << counter << std::endl;
 
-  return 0;
+    return 0;
 }
