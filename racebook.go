dd16110d7ed67bf5d68527de52979bc6a0ffa904
Everton Cavalcante <evertonranielly@gmail.com>
2026-08-12 22:01:49 -0300

    Implementation of the race condition in Go

---
+++ racebook.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var seatsAvailable = 1
+
+func bookSeat(customer string) {
+	if seatsAvailable > 0 {
+		fmt.Println(customer, "is trying to book a seat")
+		time.Sleep(50 * time.Millisecond) // Simulate some processing time
+		seatsAvailable--
+		fmt.Println(customer, "booked a seat")
+	} else {
+		fmt.Println("No seats available")
+	}
+}
+
+func main() {
+	go bookSeat("Customer A")
+	go bookSeat("Customer B")
+
+	time.Sleep(1 * time.Second) // Wait for goroutines to finish
+	fmt.Printf("Seats remaining: %d\n\n", seatsAvailable)
+}
